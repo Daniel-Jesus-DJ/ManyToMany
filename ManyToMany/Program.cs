3f84296c6332bfa326c3f780a33d65650ce3f3e5
Dietmar M <die7mar@mailbox.org>
2025-12-04 16:03:37 +0100

    fix db stuff.

--- ManyToMany/Program.cs
+++ ManyToMany/Program.cs
@@ -4,17 +4,19 @@ using ManyToMany.Core.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
-
-var conntectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
-    options.UseSqlServer(conntectionString));
+    options.UseSqlServer(connectionString));
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
+
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
