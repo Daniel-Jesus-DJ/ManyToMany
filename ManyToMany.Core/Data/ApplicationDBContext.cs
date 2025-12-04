1098d40f841a230abada58db51a99d09aeb89891
Tokto <toktoraly707@gmail.com>
2025-12-04 14:16:20 +0100

    Models Game und Person DB in app.json

---
+++ ManyToMany.Core/Data/ApplicationDBContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManyToMany.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManyToMany.Core.Data
+{
+    public  class ApplicationDBContext:DbContext 
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Game> Games { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+            modelBuilder.Entity<Person>()
+                .HasMany(p => p.Games)
+                .WithMany(g => g.Persons);
+            modelBuilder.Entity<Game>()
+                .HasMany(g => g.Persons)
+                .WithMany(p => p.Games);
+        }
+    }
+}
