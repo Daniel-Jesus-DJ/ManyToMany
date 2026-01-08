a6b0d21ed7caa2c0da4dfe5d7d6310e1cb162021
Dr. Aly <78695222+Toktoraly@users.noreply.github.com>
2026-01-08 13:52:42 +0100

    Deutsch

--- ManyToMany/Controllers/AdminController/AdminController.cs
+++ ManyToMany/Controllers/AdminController/AdminController.cs
@@ -1,51 +1,143 @@
 ﻿using ManyToMany.Core.Data;
+using ManyToMany.Core.Models;
 using ManyToMany.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace ManyToMany.Controllers
 {
-    // САМОЕ ВАЖНОЕ: Сюда пустит только если у юзера есть роль "Admin"
-    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin")] // Доступ только админам
     public class AdminController : Controller
     {
         private readonly ApplicationDBContext _context;
+        private readonly UserManager<Person> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AdminController(ApplicationDBContext context)
+        public AdminController(ApplicationDBContext context, UserManager<Person> userManager, RoleManager<IdentityRole> roleManager)
         {
             _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
+        // --- ГЛАВНАЯ СТРАНИЦА АДМИНА ---
         public async Task<IActionResult> Index()
         {
+            // Подгружаем данные для статистики M:N
+            var userGames = await _context.UserGames.Include(ug => ug.Game).Include(ug => ug.Person).ToListAsync();
+
             var model = new AdminDashboardViewModel
             {
-                Users = await _context.Users.ToListAsync(), // IdentityUser хранятся в Users
+                Users = await _userManager.Users.ToListAsync(),
                 Games = await _context.Games.Include(g => g.Genres).ToListAsync(),
                 Genres = await _context.Genres.ToListAsync(),
 
-                // Журнал покупок: кто, что и когда купил
-                AllPurchases = await _context.UserGames
-                    .Include(ug => ug.Person)
-                    .Include(ug => ug.Game)
-                    .OrderByDescending(ug => ug.PurchaseDate)
-                    .ToListAsync()
+                // Считаем статистику (M:N во всей красе)
+                GamesPopularity = userGames.GroupBy(x => x.Game.SpielName)
+                                           .ToDictionary(g => g.Key, g => g.Count()),
+
+                UsersActivity = userGames.GroupBy(x => x.Person.Email)
+                                         .ToDictionary(g => g.Key, g => g.Count())
             };
 
             return View(model);
         }
 
-        // Сюда можно добавить методы DeleteUser, EditGame и т.д.
-        // Например:
+        // --- УПРАВЛЕНИЕ ИГРАМИ ---
+
+        [HttpGet]
+        public IActionResult CreateGame() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGame(Game game)
+        {
+            _context.Games.Add(game);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditGame(int id)
+        {
+            var game = await _context.Games.FindAsync(id);
+            return View(game);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditGame(Game game)
+        {
+            _context.Games.Update(game);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteGame(int id)
+        {
+            var game = await _context.Games.FindAsync(id);
+            if (game != null)
+            {
+                _context.Games.Remove(game);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // --- УПРАВЛЕНИЕ ЖАНРАМИ ---
+
+        [HttpPost] // Быстрое создание жанра прямо с главной (сделаем форму в Index)
+        public async Task<IActionResult> CreateGenre(string genreName)
+        {
+            if (!string.IsNullOrEmpty(genreName))
+            {
+                _context.Genres.Add(new Genre { GenreName = genreName });
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteGenre(int id)
+        {
+            var genre = await _context.Genres.FindAsync(id);
+            if (genre != null)
+            {
+                _context.Genres.Remove(genre);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // --- УПРАВЛЕНИЕ ПОЛЬЗОВАТЕЛЯМИ ---
+
         [HttpPost]
         public async Task<IActionResult> DeleteUser(string id)
         {
-            var user = await _context.Users.FindAsync(id);
+            var user = await _userManager.FindByIdAsync(id);
             if (user != null)
             {
-                _context.Users.Remove(user);
-                await _context.SaveChangesAsync();
+                await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
+
+        // Назначить Админом (или снять)
+        [HttpPost]
+        public async Task<IActionResult> ToggleAdmin(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                if (await _userManager.IsInRoleAsync(user, "Admin"))
+                {
+                    await _userManager.RemoveFromRoleAsync(user, "Admin");
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(user, "Admin");
+                }
             }
             return RedirectToAction("Index");
         }
--- ManyToMany/ViewModels/AdminDashboardViewModel.cs
+++ ManyToMany/ViewModels/AdminDashboardViewModel.cs
@@ -7,6 +7,9 @@ namespace ManyToMany.ViewModels
         public List<Person> Users { get; set; }
         public List<Game> Games { get; set; }
         public List<Genre> Genres { get; set; }
-        public List<UserGame> AllPurchases { get; set; } // Журнал покупок
+
+        //N.M tables to demostrate
+        public Dictionary<string, int> GamesPopularity { get; set; } // Игра -> Кол-во покупок
+        public Dictionary<string, int> UsersActivity { get; set; }   // Юзер -> Кол-во купленных игр
     }
 }
